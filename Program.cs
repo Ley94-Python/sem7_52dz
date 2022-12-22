867cb4840215955790f63337aa814a1ef4745dde
LAY <achilova_94@list.ru>
2022-12-22 21:41:18 +0300

    решено

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+Console.WriteLine("Введите размерность двумерного массива m x n");
+Console.Write("Введите m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+double [,] matrix = new double [m,n];
+for(int i = 0; i < matrix.GetLength(0); i++)
+{
+for(int j = 0; j < matrix.GetLength(1); j++)
+{
+matrix[i,j] = new Random().Next(0,11);
+Console.Write(matrix[i,j] + " ");
+}
+Console.WriteLine();
+}
+Console.WriteLine();
+
+Console.WriteLine(matrix.GetLength(0));
+for (int j = 0; j < matrix.GetLength(1); j++)
+{
+    double sum = 0.0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        sum += matrix[i, j];
+    }
+    Console.Write($"{ Math.Round(sum / matrix.GetLength(0), 1)} ");
+}
+Console.ReadLine();
